886011ada8a66bb333f555e8566bf6d73058a0f6
Gloridust <Gloridust@gmail.com>
2023-12-11 09:02:02 +0800

    Update guessID.py

--- guessID.py
+++ guessID.py
@@ -4,19 +4,18 @@ import re
 import psutil
 import os
 
-num_threads = 8
+thread_count = 8
 
-user_input = input("请输入身份证号码，用星号 (*) 替代未知数字：")
+id_input = input("请输入身份证号码，用星号 (*) 替代未知数字：")
 
-def is_valid_id_number(n):
-    if len(str(n)) != 18:
+def is_valid_id_number(id_number):
+    if not id_number.isdigit() or len(id_number) != 18:
         return False
     factors = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
     id_remainders = ['1', '0', 'x', '9', '8', '7', '6', '5', '4', '3', '2']
-    n = str(n)
-    total_sum = sum(int(n[i]) * factors[i] for i in range(17))
+    total_sum = sum(int(id_number[i]) * factors[i] for i in range(17))
     remainder = total_sum % 11
-    return id_remainders[remainder] == str(n[17])
+    return id_remainders[remainder] == str(id_number[17])
 
 disable_memory = False
 
@@ -28,31 +27,26 @@ def get_memory_usage():
         print('>>> 正在将数据集加载到内存中，已使用 (MB)：', memory, end='\r')
 
 year_days = {
-    '01': [x for x in range(1, 32)],
-    '02': [x for x in range(1, 29)],
-    '03': [x for x in range(1, 32)],
-    '04': [x for x in range(1, 31)],
-    '05': [x for x in range(1, 32)],
-    '06': [x for x in range(1, 31)],
-    '07': [x for x in range(1, 32)],
-    '08': [x for x in range(1, 32)],
-    '09': [x for x in range(1, 31)],
-    '10': [x for x in range(1, 32)],
-    '11': [x for x in range(1, 31)],
-    '12': [x for x in range(1, 32)]
+    '01': [str(x).zfill(2) for x in range(1, 32)],
+    '02': [str(x).zfill(2) for x in range(1, 29)],
+    '03': [str(x).zfill(2) for x in range(1, 32)],
+    '04': [str(x).zfill(2) for x in range(1, 31)],
+    '05': [str(x).zfill(2) for x in range(1, 32)],
+    '06': [str(x).zfill(2) for x in range(1, 31)],
+    '07': [str(x).zfill(2) for x in range(1, 32)],
+    '08': [str(x).zfill(2) for x in range(1, 32)],
+    '09': [str(x).zfill(2) for x in range(1, 31)],
+    '10': [str(x).zfill(2) for x in range(1, 32)],
+    '11': [str(x).zfill(2) for x in range(1, 31)],
+    '12': [str(x).zfill(2) for x in range(1, 32)]
 }
 
-for k, v in year_days.items():
-    for i in range(len(v)):
-        if v[i] < 10:
-            v[i] = '0' + str(v[i])
-
-if len(user_input) != 18:
-    print(len(user_input))
+if len(id_input) != 18:
+    print(len(id_input))
     print('>>> 请输入18位身份证号码，并用星号 (*) 替代缺失的数字。')
     exit()
 
-count_asterisks = user_input.count('*')
+count_asterisks = id_input.count('*')
 if count_asterisks > 18:
     print('>>> 星号的数量不能超过18个。')
     exit()
@@ -61,11 +55,11 @@ if count_asterisks == 0:
     print('>>> 没有缺失的数字。')
     exit()
 
-city_code = user_input[0:6]
-birth_year = user_input[6:10]
-birth_month = user_input[10:12]
-birth_day = user_input[12:14]
-tail_digits = user_input[14:18]
+city_code = id_input[0:6]
+birth_year = id_input[6:10]
+birth_month = id_input[10:12]
+birth_day = id_input[12:14]
+tail_digits = id_input[14:18]
 
 print('>>> 城市代码:', city_code)
 print('>>> 出生年份:', birth_year)
@@ -74,13 +68,13 @@ print('>>> 出生日期:', birth_day)
 print('>>> 顺序号:', tail_digits)
 
 cities = None
-with open('citycodes.txt', mode='r', encoding='utf-8') as f:
+with open('citycodes.txt', 'r', encoding='utf-8') as f:
     cities = f.read().split('\n')
 
 valid_cities = []
-city_pattern = city_code.replace('*', '(\d)')
+city_pattern = city_code.replace('*', r'(\d)')
 for city in cities:
-    if re.match(city_pattern, city):
+    if re.search(city_pattern, city):
         valid_cities.append(city)
 
 if len(valid_cities) == 0:
@@ -93,7 +87,7 @@ birth_year_options = []
 if birth_year == '****':
     birth_year_options = [x for x in range(1949, time.localtime().tm_year + 1)]
 else:
-    birth_year_pattern = birth_year.replace('*', '(\d)')
+    birth_year_pattern = birth_year.replace('*', r'(\d)')
     birth_year_options = [i for i in range(1949, time.localtime().tm_year + 1) if re.match(birth_year_pattern, str(i))]
 
 print('>>> 有效的出生年份:', birth_year_options)
@@ -104,11 +98,10 @@ if birth_month == '00':
 
 birth_month_options = []
 if birth_month == '**':
-    birth_month_options = [x for x in range(1, 13)]
-    birth_month_options = [str(x).zfill(2) for x in birth_month_options]
+    birth_month_options = [str(x).zfill(2) for x in range(1, 13)]
 else:
     if birth_month[0] == '*' and birth_month[1] != '*':
-        birth_month_options = [str(x).zfill(2) for x in [1, 11, 12] if int(birth_month[1]) == x // 10]
+        birth_month_options = [str(x).zfill(2) for x in range(1, 13) if int(birth_month[1]) == x // 10]
     elif birth_month[0] != '*' and birth_month[1] == '*':
         birth_month_options = [str(x).zfill(2) for x in range(10, 13) if int(birth_month[0]) == x // 10]
     else:
@@ -123,7 +116,7 @@ for month_option in birth_month_options:
         birth_day_options[month_option] = year_days[month_option]
     else:
         if birth_day[0] == '*' and birth_day[1] != '*':
-            birth_day_options[month_option] = [str(x).zfill(2) for x in [1, 11, 21] if int(birth_day[1]) == x // 10]
+            birth_day_options[month_option] = [str(x).zfill(2) for x in range(1, 32) if int(birth_day[1]) == x // 10]
         elif birth_day[0] != '*' and birth_day[1] == '*':
             if int(birth_day[0]) == 1:
                 birth_day_options[month_option] = [str(x).zfill(2) for x in range(10, 20)]
@@ -175,43 +168,35 @@ disable_memory = False
 all_count = len(all_options)
 print('>>> 数据集大小:', all_count)
 lock = threading.Lock()
-index = []
 cache = []
 
-def split_and_verify_range(option_range, index_index):
-    global index
-    global cache
+def split_and_verify_range(option_range):
+    result = []
     for option in option_range:
-        index[index_index] += 1
         real_id = str(option)
         if is_valid_id_number(real_id):
-            cache[index_index].append(real_id)
+            result.append(real_id)
+    return result
 
-range_size = all_count // num_threads
+range_size = all_count // thread_count
 option_ranges = [all_options[i:i+range_size] for i in range(0, len(all_options), range_size)]
 
 start_time = time.time()
-for i in range(num_threads):
-    index.append(0)
-    cache.append([])
-    t = threading.Thread(target=split_and_verify_range, args=(option_ranges[i], i))
+threads = []
+for option_range in option_ranges:
+    t = threading.Thread(target=lambda: cache.extend(split_and_verify_range(option_range)))
     t.start()
+    threads.append(t)
 
-all_options = []
-time.sleep(1)
+for t in threads:
+    t.join()
+
 disable_memory = True
 time.sleep(1)
-
 os.system('cls')
-while True:
-    if threading.active_count() == 1:
-        print('>>> 所有线程已完成')
-        print('>>> 耗时:', time.time() - start_time)
-        result = "\n".join(option for sublist in cache for option in sublist)
-        with open('resultID.txt', 'a') as f:
-            f.write(result)
-        break
-    else:
-        time.sleep(0.5)
-        total = sum(i for i in index)
-        print('进度:', f'{total}/{all_count}  {round(total/all_count*100, 2)}%', end='\r')
+
+print('>>> 所有线程已完成')
+print('>>> 耗时:', time.time() - start_time)
+result = "\n".join(cache)
+with open('resultID.txt', 'a') as f:
+    f.write(result)
